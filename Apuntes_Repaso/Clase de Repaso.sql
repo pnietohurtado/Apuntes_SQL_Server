4ae5e82fc02f978807b9d6d30d6313cf6e75b598
Pablo Nieto Hurtado <pnietohurtado03@gmail.com>
2025-10-12 19:16:17 +0200

    End of the summary SELECT module

--- Apuntes_Repaso/Clase de Repaso.sql
+++ Apuntes_Repaso/Clase de Repaso.sql
@@ -22,5 +22,22 @@ SELECT
 	COUNT(*) 
 FROM tblEmployee e
 GROUP BY LEFT(e.EmployeeFirstName,1), e.EmployeeLastName
-HAVING COUNT(*) >= 50
-ORDER BY e.EmployeeLastName DESC
+-- HAVING COUNT(*) >= 50
+ORDER BY Initial DESC
+
+
+
+SELECT 
+	DATENAME(MONTH, DateOfBirth) AS MonthNumber, 
+	COUNT(*) AS NumberEmployees, 
+	YEAR(DateOfBirth) 
+FROM tblEmployee
+GROUP BY DATENAME(MONTH, DateOfBirth ) , DATEPART(MONTH, DateOfBirth), YEAR(DateOfBirth)
+ORDER BY DATEPART(MONTH, DateOfBirth)
+-- *** You can't order by a value that is not in the SELECT statement, threrefore you have to 
+-- include that "variable/data" into the SELECT or the GROUP BY. The problem is that adding a new 
+-- data of column will bother our statement result, that's why we added that in the GROUP BY where it doesn't 
+-- upset the result.
+
+
+
