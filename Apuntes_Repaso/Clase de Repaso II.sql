b54880ed810a4155cbcd23a4903133f47ced35f6
Pablo Nieto Hurtado <pnietohurtado03@gmail.com>
2025-10-19 19:30:21 +0200

    Seguimos mañana con la siguiente clase

--- Apuntes_Repaso/Clase de Repaso II.sql
+++ Apuntes_Repaso/Clase de Repaso II.sql
@@ -20,3 +20,32 @@ FROM tblEmployee e
 	JOIN tblTransaction t ON e.EmployeeNumber = t.EmployeeNumber
 GROUP BY e.EmployeeFirstName, e.EmployeeLastName
 HAVING SUM(t.Amount) > 0
+
+
+SELECT 
+	e.EmployeeFirstName AS FirstName, 
+	e.EmployeeLastName AS LasName, 
+	SUM(t.Amount) AS Total 
+FROM tblEmployee e
+	JOIN tblTransaction t ON e.EmployeeNumber = t.EmployeeNumber
+WHERE e.EmployeeNumber = 1046
+GROUP BY e.EmployeeFirstName, e.EmployeeLastName
+
+
+SELECT * FROM tblTransaction WHERE tblTransaction.EmployeeNumber = 1046
+
+-- No hay 1046 EmployeeNumber, porque no hay transacción de este empleado en la tabla de transacciones 
+
+
+
+-- Para incluir a este empleado hacemos los siguiente 
+
+SELECT 
+	e.EmployeeNumber AS Number, 
+	e.EmployeeFirstName AS FirstName, 
+	e.EmployeeLastName AS LasName, 
+	SUM(t.Amount) AS Total 
+FROM tblEmployee e
+	LEFT JOIN tblTransaction t ON e.EmployeeNumber = t.EmployeeNumber
+WHERE e.EmployeeNumber > 1010
+GROUP BY e.EmployeeFirstName, e.EmployeeLastName, e.EmployeeNumber
