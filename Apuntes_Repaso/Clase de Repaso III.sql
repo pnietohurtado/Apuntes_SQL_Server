df9683524e5a2385619cb6bafef4983be02a5dca
Pablo Nieto Hurtado <pnietohurtado03@gmail.com>
2025-10-27 17:07:05 +0100

    Practica join

--- Apuntes_Repaso/Clase de Repaso III.sql
+++ Apuntes_Repaso/Clase de Repaso III.sql
@@ -13,3 +13,25 @@ WHERE EmployeeNumber = 3
 --SELECT * FROM tblTransaction WHERE EmployeeNumber = 194
 
 ROLLBACK TRAN 
+
+
+
+
+SELECT
+	t.EmployeeNumber AS TEmployeeNumber, 
+	e.EmployeeNumber AS EEmployeeNumber, 
+	SUM(Amount) AS SumAmount
+FROM tblTransaction t
+		left JOIN tblEmployee e ON t.EmployeeNumber = e.EmployeeNumber
+GROUP BY t.EmployeeNumber, e.EmployeeNumber
+ORDER BY EEmployeeNumber
+
+
+
+BEGIN TRAN 
+
+UPDATE tblEmployee SET DateOfBirth = '2101-01-02' 
+WHERE EmployeeNumber = 537
+SELECT * FROM tblEmployee ORDER BY DateOfBirth DESC
+
+ROLLBACK TRAN 
