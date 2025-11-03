9681513fe6571b428aed500ed3994799386c424f
Pablo Nieto Hurtado <pnietohurtado03@gmail.com>
2025-11-03 18:15:35 +0100

    Primera parte de la clase

--- Apuntes_Repaso/Clase de Repaso III.sql
+++ Apuntes_Repaso/Clase de Repaso III.sql
@@ -35,3 +35,18 @@ WHERE EmployeeNumber = 537
 SELECT * FROM tblEmployee ORDER BY DateOfBirth DESC
 
 ROLLBACK TRAN 
+
+
+
+
+SELECT 
+		e.EmployeeGovernmentID AS ID ,
+		COUNT(e.EmployeeGovernmentID) AS MyCount 
+FROM tblEmployee e
+GROUP BY e.EmployeeGovernmentID
+HAVING COUNT(e.EmployeeGovernmentID) > 1
+
+
+SELECT * FROM tblEmployee WHERE EmployeeGovernmentID IN ('HN513777D')
+SELECT * FROM tblEmployee
+
